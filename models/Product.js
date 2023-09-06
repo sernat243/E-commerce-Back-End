e2d2410beeb2f7302760bac34a4cd1df2d9cdf72
Gabriel <sernat243@gmail.com>
2023-09-06 13:11:32 -0600

    defined model associations, debugged product.js typo, added sequelize sync to server.js

--- models/Product.js
+++ models/Product.js
@@ -12,7 +12,7 @@ Product.init(
   {
     // define columns
     id: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
